9ebb5273b23cd3813e538c25669958b6c03372a2
Holy AraHippo <theholyarahippo@gmail.com>
2026-01-14 17:57:44 +0100

    Removed test files for now.

--- test5.py
+++
@@ -1,9 +0,0 @@
-from microps import js
-
-parent = js.Object()
-parent.species = "Hippo"
-
-child = js.Object()
-child.__proto__ = parent # Set the prototype in C memory
-
-print(child.species) # JS('Hippo') <- Inherited through the lie!
